b1577d4554db0e4b644ac238c59151540acaebf3
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-08-24 12:06:06 +0300

    DTO

---
+++ NORTHWIND/NorthwinProject.Entities/Order.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwinProject.Entities
+{
+    public class Order
+    {
+        public Order()
+        {
+            OrderDetails = new List<OrderDetail>();
+        }
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+
+        public int? EmployeeID { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+
+        public int? ShipVia { get; set; }
+
+        public decimal? Freight { get; set; }
+
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipRegion { get; set; }
+        public string ShipPostalCode { get; set; }
+        public string ShipCountry { get; set; }
+
+
+        //neden set yok ?
+        //yazdığımız sınıfta koleksiyon varsa bu mutlaka bu koleksiyonu constructorda initialize et yani instanceyi al 
+        //dışardan kullanıma hazır olmalı diğer türlü Null exceptionu alıyoruz
+        //set kapanıyor çünkü dışardan müdehale edilmemesi gerekiyor burada instaceyi alıyoruz 
+        //fakat dışardan null değer atanıyor aynı hatayı tekrar almamıza sebep oluyor 
+        //orderDetails=...... kapatıyoruz yani referansı değiştirmeyi 
+
+        //orderDetails.Clear(); vb durumlar kullanılabilir 
+        public List<OrderDetail> OrderDetails { get; }
+
+        ////get hali arka planda bu şekilde çalışıyor
+        //private readonly List<OrderDetail> _orderDetails;
+        //public List<OrderDetail> OrderDetails
+        //{
+        //    get
+        //    {
+        //        return _orderDetails;
+        //    }
+        //}
+    }
+}
---
+++ NORTHWIND/NorthwinProject.Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwinProject.Entities
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+
+        public int? CategoryID { get; set; }
+        public int? SupplierID { get; set; }
+
+        public decimal? UnitPrice { get; set; }
+        public string? QuantityPerUnit { get; set; }
+
+        public short? UnitsInstock { get; set; }
+        public short? UnitsOnOrder { get; set; }
+
+        public short? ReorderLevel { get; set; }
+
+        public bool Discontinued { get; set; }
+    }
+
+}
