36ca0b3929e5fb60bebbcc65aa4bb25b589df2e9
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-08-25 09:35:45 +0300

    CategoryUpdate added

---
+++ NORTHWIND/NorthwindProject.WinForms/CategoryUpdateForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace NorthwindProject.WinForms
+{
+    partial class CategoryUpdateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnUpdate = new Button();
+            txtDescription = new TextBox();
+            txtCategoryName = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(371, 211);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(75, 23);
+            btnUpdate.TabIndex = 13;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // txtDescription
+            // 
+            txtDescription.Location = new Point(280, 161);
+            txtDescription.Name = "txtDescription";
+            txtDescription.Size = new Size(166, 23);
+            txtDescription.TabIndex = 12;
+            // 
+            // txtCategoryName
+            // 
+            txtCategoryName.Location = new Point(280, 117);
+            txtCategoryName.Name = "txtCategoryName";
+            txtCategoryName.Size = new Size(166, 23);
+            txtCategoryName.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(280, 77);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(166, 23);
+            textBox1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(206, 169);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Açıklama";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(206, 125);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Kategori adı";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(206, 80);
+            label1.Name = "label1";
+            label1.Size = new Size(18, 15);
+            label1.TabIndex = 7;
+            label1.Text = "ID";
+            // 
+            // CategoryUpdateForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnUpdate);
+            Controls.Add(txtDescription);
+            Controls.Add(txtCategoryName);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "CategoryUpdateForm";
+            Text = "CategoryUpdateForm";
+            Load += CategoryUpdateForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnUpdate;
+        private TextBox txtDescription;
+        private TextBox txtCategoryName;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+    }
+}
---
+++ NORTHWIND/NorthwindProject.WinForms/CategoryUpdateForm.cs
@@ -0,0 +1,49 @@
+﻿using NorthwindProject.DataAccess;
+using NorthwindProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NorthwindProject.WinForms
+{
+    public partial class CategoryUpdateForm : Form
+    {
+        private int _categoryid;
+        public CategoryUpdateForm(int CategoryId)
+        {
+
+            InitializeComponent();
+            _categoryid = CategoryId;
+        }
+
+        private void CategoryUpdateForm_Load(object sender, EventArgs e)
+        {
+            var repostry = new CategoryRepository();
+            var category = repostry.Find(_categoryid);
+            if (category != null)
+            {
+                txtCategoryName.Text = category.CategoryName;
+                txtDescription.Text = category.Description;
+                textBox1.Text = category.CategoryID.ToString();
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            var category = new Category();
+            category.CategoryID = _categoryid;
+            category.CategoryName = txtCategoryName.Text;
+            category.Description = txtDescription.Text;
+
+            var repostry = new CategoryRepository();
+            repostry.Update(category);
+        }
+    }
+}
